9a256922cf6ffa786e125ada279d04cc6d11be48
Truong Quang Tinh <truongquangtinh1997@gmail.com>
2024-08-21 23:52:56 +0700

    update app

---
+++ terraform/dev/dev_app2/main.tf
@@ -0,0 +1,65 @@
+
+
+resource "aws_security_group" "instance" {
+  name = "security-group-test"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_instance" "example" {
+  ami                    = data.aws_ami.amazon.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+}
+
+data "aws_ami" "amazon" {
+  most_recent = true
+
+  owners = ["amazon"]
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["*ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+data "aws_vpc" "default" {
+  default = true
+}
